4b665288670749d681763bd077c99f320b9553f7
buggiman6969 <abdullaapon12@gmail.com>
2023-02-14 10:48:21 +0600

    done

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,3 +1,6 @@
+const furitTittle = document.getElementById("fruits-tittle")
+furitTittle.innerText = "fruit name change my javascript";
+
 const places = document.getElementsByClassName("imp-places");
 for(const place of places){
 console.log(place);
